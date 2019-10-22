062c0f7d16f02523fb5a64b8c82bbded04ab5c28
malhan niazi <m.malhan.niazi@gmail.com>
2019-10-22 14:28:16 -0400

    initial form for the affordability calculator

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AffordabilityCalculator msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AffordabilityCalculator from "./components/AffordabilityCalculator.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    AffordabilityCalculator
   }
 };
 </script>
@@ -20,8 +20,8 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 14px;
 }
 </style>
---
+++ src/components/AffordabilityCalculator.vue
@@ -0,0 +1,267 @@
+<template>
+  <div class="hello">
+    <div class="container">
+      <div class="col">
+        <div class="row">
+          <div class="col">
+            <p>Affordability Calculator</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <p>Figure out the maximum home price you can afford before you start house hunting.</p>
+            <p>Note: Calculators display default values. Enter new figures to override.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6" id="input">
+            <form action>
+              <div class="form-group" id="gross-income-input-group">
+                <label for="gross-income-input-field">Gross Income</label>
+                <div class="input-group mb-2">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">$</div>
+                  </div>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="gross-income-input-field"
+                    value="4000"
+                  />
+                  <div class="input-group-append">
+                    <div class="input-group-text">/mo</div>
+                  </div>
+                  <input
+                    type="range"
+                    class="custom-range"
+                    min="500"
+                    max="50000"
+                    value="4000"
+                    id="gross-income-input-range"
+                  />
+                </div>
+              </div>
+              <div class="form-group" id="car-loan-input-group">
+                <label for="car-loan-input-field">Car Loan</label>
+                <div class="input-group mb-2">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">$</div>
+                  </div>
+                  <input type="text" class="form-control" id="car-loan-input-field" value="500" />
+                  <div class="input-group-append">
+                    <div class="input-group-text">/mo</div>
+                  </div>
+                  <input
+                    type="range"
+                    class="custom-range"
+                    min="0"
+                    max="5000"
+                    value="500"
+                    id="car-loan-input-range"
+                  />
+                </div>
+              </div>
+              <div class="form-group" id="credit-card-input-group">
+                <label for="credit-card-input-field">Credit Cards</label>
+                <div class="input-group mb-2">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">$</div>
+                  </div>
+                  <input type="text" class="form-control" id="credit-card-input-field" value="100" />
+                  <div class="input-group-append">
+                    <div class="input-group-text">/mo</div>
+                  </div>
+                  <input
+                    type="range"
+                    class="custom-range"
+                    min="0"
+                    max="5000"
+                    value="100"
+                    id="credit-card-input-range"
+                  />
+                </div>
+              </div>
+              <div class="form-group" id="other-debts-input-group">
+                <label for="other-debts-input-field">Student Loan/Other Debts</label>
+                <div class="input-group mb-2">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">$</div>
+                  </div>
+                  <input type="text" class="form-control" id="other-debts-input-field" value="150" />
+                  <div class="input-group-append">
+                    <div class="input-group-text">/mo</div>
+                  </div>
+                  <input
+                    type="range"
+                    class="custom-range"
+                    min="0"
+                    max="5000"
+                    value="150"
+                    id="other-debts-input-range"
+                  />
+                </div>
+              </div>
+              <div class="form-group" id="down-payment-input-group">
+                <label for="down-payment-input-field">Down Payment</label>
+                <div class="input-group mb-2">
+                  <input type="text" class="form-control" id="down-payment-input-field" value="5" />
+                  <div class="input-group-append">
+                    <div class="input-group-text">%</div>
+                  </div>
+                  <input
+                    type="range"
+                    class="custom-range"
+                    min="3"
+                    max="50"
+                    value="5"
+                    id="down-payment-input-range"
+                  />
+                </div>
+              </div>
+              <div class="form-group" id="interest-rate-input-group">
+                <label for="interest-rate-input-field">Interest Rate</label>
+                <div class="input-group mb-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="interest-rate-input-field"
+                    value="4.25"
+                  />
+                  <div class="input-group-append">
+                    <div class="input-group-text">%</div>
+                  </div>
+                  <input
+                    type="range"
+                    class="custom-range"
+                    min="2.5"
+                    max="11"
+                    value="4.25"
+                    id="interest-rate-input-range"
+                  />
+                </div>
+              </div>
+              <div class="form-group" id="term-input-group">
+                <label for="term-input-select">Term</label>
+                <div class="input-group mb-2">
+                  <select class="form-control" id="term-input-select">
+                    <option>10</option>
+                    <option>15</option>
+                    <option>20</option>
+                    <option>25</option>
+                    <option selected>30</option>
+                  </select>
+                  <div class="input-group-append">
+                    <div class="input-group-text">years</div>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group" id="hoi-input-group">
+                <label for="hoi-input-field">Homeowner's Insurance</label>
+                <div class="input-group mb-2">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">$</div>
+                  </div>
+                  <input type="text" class="form-control" id="hoi-input-field" value="1200" />
+                  <div class="input-group-append">
+                    <div class="input-group-text">/yr</div>
+                  </div>
+                  <input
+                    type="range"
+                    class="custom-range"
+                    min="0"
+                    max="4000"
+                    value="1200"
+                    id="hoi-input-range"
+                  />
+                </div>
+              </div>
+              <div class="form-group" id="hoa-input-group">
+                <label for="hoa-input-field">Homeowner's Association (HOA) or Condo Fees</label>
+                <div class="input-group mb-2">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">$</div>
+                  </div>
+                  <input type="text" class="form-control" id="hoa-input-field" value="50" />
+                  <div class="input-group-append">
+                    <div class="input-group-text">/mo</div>
+                  </div>
+                  <input
+                    type="range"
+                    class="custom-range"
+                    min="5"
+                    max="600"
+                    value="50"
+                    id="hoa-input-range"
+                  />
+                </div>
+              </div>
+              <div class="form-group" id="property-tax-input-group">
+                <label for="property-tax-input-field">Property Taxes</label>
+                <div class="input-group mb-2">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">$</div>
+                  </div>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="property-tax-input-field"
+                    value="1500"
+                  />
+                  <div class="input-group-append">
+                    <div class="input-group-text">/yr</div>
+                  </div>
+                  <input
+                    type="range"
+                    class="custom-range"
+                    min="400"
+                    max="25000"
+                    value="1500"
+                    id="property-tax-input-range"
+                  />
+                </div>
+              </div>
+              <div class="form-group" id="form-mod-group">
+                <div class="row">
+                  <div class="col">
+                    <button type="button" class="btn btn-secondary btn-block">Simple View</button>
+                  </div>
+                  <div class="col">
+                    <button type="button" class="btn btn-secondary btn-block">Reset</button>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group" id="submit-form-group">
+                <button class="btn btn-block btn-primary" type="submit">See Results</button>
+              </div>
+            </form>
+          </div>
+          <div class="col-6" id="result"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AffordabilityCalculator"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
